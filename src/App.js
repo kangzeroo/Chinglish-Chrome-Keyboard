df3ef4305d103f248e95e56d7c20fb9d1040f135
kangzeroo <kangzeroo@gmail.com>
2019-08-25 00:41:31 -0400

    demoable in working state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,148 @@
 import React, { useState , useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { fromEvent } from 'rxjs';
 import PredictionBar from './components/PredictionBar'
-import getType from 'pinyin-or-hanzi'
 import { Helmet } from 'react-helmet'
 import { Input } from 'antd'
+import * as _ from 'lodash'
+import firebase from 'firebase'
+
+const hardcodedSuggestions = [
+  {id: "1", mandarin: '我', english: 'I', pinyin: 'wo'},
+  {id: "2", mandarin: '的', english: 'donut', pinyin: 'wa'},
+  {id: "3", mandarin: '猫', english: 'living', pinyin: 'whuo'},
+  {id: "4", mandarin: '喜', english: 'stateful', pinyin: 'wu'},
+  {id: "5", mandarin: '欢', english: 'cord', pinyin: 'whoo'},
+  {id: "6", mandarin: '喝', english: 'exhaust', pinyin: 'woo'},
+  {id: "7", mandarin: '牛', english: 'nope', pinyin: 'whoa'},
+  {id: "8", mandarin: '奶', english: 'every', pinyin: 'wha'},
+  {id: "9", mandarin: '种', english: 'innards', pinyin: 'who'},
+]
+
+const generateSuggestions = () => {
+  return _.shuffle(hardcodedSuggestions)
+}
+
+// const isNumber = (str) => {
+//   const REGEX_NUMBER = /^\d+$/u;
+//   return REGEX_NUMBER.test(str)
+// }
+const isEnglish = (str) => {
+  const REGEX_ENGLISH = /([a-zA-Z]+)|(\s)/u;
+  return REGEX_ENGLISH.test(str);
+}
+
+const determineConstantVariableSplit = (text) => {
+  // const isChinese = (str) => {
+  //   const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
+  //   return REGEX_CHINESE.test(str);
+  // }
+  
+  // const isEmoji = (str) => {
+  //   const REGEX_EMOJI = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/u;
+  //   return REGEX_EMOJI.test(str);
+  // }
+  let latestNonEnglish = 0
+  text.split('').forEach((char, idx) => {
+    if (!isEnglish(char)) {
+      latestNonEnglish = idx + 1
+    }
+  })
+  return latestNonEnglish
+}
 
 const App = () => {
 
+  // the strings
   const [fullString, setFullString] = useState("")
-  const [decidedString, setDecidedString] = useState("")
-  const [decidingString, setDecidingString] = useState("")
+  const [constantString, setConstantString] = useState("")
+  const [variableString, setVariableString] = useState("")
+  // handles an edge case where the event listener keypress and the native input onChange results in onChange entering in a duplicated number
+  // to see this edge case, simply remove `if (numbersAllowed)` anywhere you see it in this file
+  const [numbersAllowed, setNumbersAllowed] = useState(true)
+  // the estimated values
+  const [suggestions, setSuggestions] = useState([])
 
-  const continueChinese = (e) => {
-    const text = e.target.value
-    setFullString(text)
-    const undecidedPosition = decidedString.length
-    setDecidingString(text.slice(undecidedPosition))
-    console.log(text.slice(undecidedPosition))
+  useEffect(() => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBCMo2Peyseb9xXZSBIV2uChXToGg3Y68g",
+      authDomain: "chinglish-keyboard.firebaseapp.com",
+      databaseURL: "https://chinglish-keyboard.firebaseio.com",
+      projectId: "chinglish-keyboard",
+      storageBucket: "",
+      messagingSenderId: "915727899842",
+      appId: "1:915727899842:web:3b8a1636f9260633"
+    })
+  }, [])
+
+  useEffect(() => {
+    const input = document.getElementById("input-bar")
+    const keyPresses = fromEvent(input, 'keypress');
+    const subscription = keyPresses.subscribe(e => {
+      console.log(e)
+      console.log(variableString)
+      if (!e.shiftKey && !e.ctrlKey && !e.altKey && variableString && variableString !== " ") {
+        if (e.code === "Digit1") {
+          selectChinese(suggestions[0].mandarin)
+        } else if (e.code === "Digit2") {
+            selectChinese(suggestions[1].mandarin)
+        } else if (e.code === "Digit3") {
+            selectChinese(suggestions[2].mandarin)
+        } else if (e.code === "Digit4") {
+            selectChinese(suggestions[3].mandarin)
+        } else if (e.code === "Digit5") {
+            selectChinese(suggestions[4].mandarin)
+        } else if (e.code === "Digit6") {
+            selectChinese(suggestions[5].mandarin)
+        } else if (e.code === "Digit7") {
+            selectChinese(suggestions[6].mandarin)
+        } else if (e.code === "Digit8") {
+            selectChinese(suggestions[7].mandarin)
+        } else if (e.code === "Digit9") {
+            selectChinese(suggestions[8].mandarin)
+        }
+      }
+    });
+    return () => subscription.unsubscribe()
+  }, [variableString])
+  
+  const continueChinese = async (e) => {
+    console.log(e)
+
+    let text = e.target.value
+
+    const latestNonEnglish = determineConstantVariableSplit(text)
+    setConstantString(text.slice(0,latestNonEnglish))
+    const varStr = text.slice(latestNonEnglish)
+    setVariableString(varStr)
+
+    if (isEnglish(varStr) && varStr !== " ") {
+      setSuggestions(generateSuggestions())
+    }
+    if (!text) {
+      setSuggestions([])
+    }
+
+    if (numbersAllowed) {
+      setFullString(text)
+    }
+
+    console.log(`TEXT: `, text)
+    console.log(`CONSTANT: `, text.slice(0,latestNonEnglish))
+    console.log(`VARIABLE: `, text.slice(latestNonEnglish))
   }
 
   const selectChinese = (chineseText) => {
-    setFullString(`${decidedString}${chineseText}`)
-    setDecidedString(`${decidedString}${chineseText}`)
-    setDecidingString("")
+    let newText = `${constantString}${variableString[0] === " " ? " " : ""}${chineseText}`
+    console.log('newText: ', newText)
+    setFullString(newText)
+    setConstantString(`${constantString}${chineseText}`)
+    setVariableString("")
+    setNumbersAllowed(false)
+    setTimeout(() => {
+      setNumbersAllowed(true)
+      setSuggestions([])
+    },100)
     document.getElementById("input-bar").focus()
   }
 
@@ -31,15 +150,16 @@ const App = () => {
     <div className="App">
       <Helmet>
           <meta charSet="utf-8" />
-          <title>Chinglish</title>
+          <title>Chinglish Keyboard</title>
+          <link rel="shortcut icon" type="image/png" href="https://www.emojimeaning.com/img/img-facebook-64/1f472.png"/>
       </Helmet>
       <header className="App-header">
-        <PredictionBar wordString={decidingString} selectChinese={selectChinese} />
+        <PredictionBar wordString={variableString} selectChinese={selectChinese} suggestions={suggestions} />
         <br/>
         <Input
           id="input-bar"
           placeholder="Type Pinyin..."
-          style={{ fontSize: '16px', padding: '10px', width: '500px' }}
+          style={{ fontSize: '16px', padding: '10px', width: '500px', position: 'absolute', bottom: '50px', left: '30vw' }}
           value={fullString}
           onChange={(e) => continueChinese(e)}
         />
--- src/components/PredictionBar.js
+++ src/components/PredictionBar.js
@@ -1,51 +1,23 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
-const PredictionBar = ({ wordString, selectChinese }) => {
-
-    const predictions = [
-        {id: "1", value: '我'},
-        {id: "2", value: '的'},
-        {id: "3", value: '猫'},
-        {id: "4", value: '喜'},
-        {id: "5", value: '欢'},
-        {id: "6", value: '喝'},
-        {id: "7", value: '牛'},
-        {id: "8", value: '奶'},
-    ]
-
-    useEffect(() => {
-        document.addEventListener('keyup', (e) => {
-            if (e.code === "Digit1") {
-                selectChinese(predictions[0].value)
-            } else if (e.code === "Digit2") {
-                selectChinese(predictions[1].value)
-            } else if (e.code === "Digit3") {
-                selectChinese(predictions[2].value)
-            } else if (e.code === "Digit4") {
-                selectChinese(predictions[3].value)
-            } else if (e.code === "Digit5") {
-                selectChinese(predictions[4].value)
-            } else if (e.code === "Digit6") {
-                selectChinese(predictions[5].value)
-            } else if (e.code === "Digit7") {
-                selectChinese(predictions[6].value)
-            } else if (e.code === "Digit8") {
-                selectChinese(predictions[7].value)
-            } else if (e.code === "Digit9") {
-                selectChinese(predictions[8].value)
-            }
-        });
-    }, [])
-    
+const PredictionBar = ({ wordString, selectChinese, suggestions }) => {
     return (
-        <section style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start' }}>
+        <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
             {
-                predictions.map((word, idx) => (
-                    <div key={word.id} onClick={() => selectChinese(word.value)} style={{ padding: '5px', border: '1px solid rgba(256,256,256,0.5)', cursor: 'pointer' }}>
-                        <span style={{ fontSize: '5px' }}>{idx + 1}</span>
-                        <span style={{ fontSize: '20px', fontWeight: 'bold', margin: '5px' }}>{word.value}</span>
+                suggestions.map((word, idx) => (
+                    <div key={word.id} onClick={() => selectChinese(word.mandarin)} style={{ padding: '5px 5px 5px 10px', border: '1px solid rgba(256,256,256,0.5)', cursor: 'pointer', minWidth: '100px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
+                        <span style={{ fontSize: '5px' }}>{idx + 1} &nbsp;</span>
+                        <span style={{ fontSize: '20px', fontWeight: 'bold', margin: '5px' }}>{word.mandarin}</span>
+                        <span style={{ fontSize: '10px', fontWeight: 'bold', margin: '5px' }}>{word.english}</span>
                     </div>
                 ))
+            }
+            {
+                suggestions.length === 0
+                ?
+                <img src="https://www.fluentu.com/blog/chinese/wp-content/uploads/2016/05/learn-chinese-facebook-1.png" style={{ width: '250px', height: 'auto' }} />
+                :
+                null
             }
         </section>
     )
